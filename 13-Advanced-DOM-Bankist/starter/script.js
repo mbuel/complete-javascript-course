65f3b6249562a6a3faedd1f369f59da317a9a0e6
mbuel <morris.buel@gmail.com>
2021-03-01 15:07:39 -0800

    finishing up sec 14

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,19 +7,22 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const allSections = document.querySelectorAll('.section');
+const allButtons = document.getElementsByTagName('button');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = function (e) {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(el =>  
+  el.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,267 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+document.getElementsByTagName('header')[0].append(message);
+
+console.log(message.style.color);
+message.addEventListener('click', () => {
+  message.remove();
+});
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const changeView = function(view) {
+  const viewPort = document.querySelector(`${view}`);
+  viewPort.scrollIntoView({behavior: 'smooth'});
+}
+
+btnScrollTo.addEventListener('click', () => {
+  changeView('#section--1')
+});
+
+// Focused per event handler
+// document.querySelectorAll('.nav__link').forEach((el) => {
+//   el.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     changeView(id);
+//   });
+// });
+
+// Delegated handler
+document.querySelector('.nav__links').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    changeView(id);
+  }
+})
+
+const h1 = document.querySelector('h1');
+
+// Going Downwards
+
+console.log(h1.querySelectorAll('.highlight'));
+
+const header = document.querySelector('.header');
+
+console.log(header);
+
+
+
+// h1.addEventListener('mouseenter', (e) => console.log(e));
+
+// h1.onmouseenter = function (e) {
+//   console.warn('test');
+// }
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
+
+console.log(randomColor());
+
+header.style.background = 'var (--gradient-secondary)';
+
+let activeTab = '1';
+
+document.querySelector('.operations').addEventListener('click', (e)=> {
+  const closestTab = e.target.closest('.operations__tab');
+  console.log(closestTab);
+  if (!closestTab) return;
+  if((closestTab.getAttribute('data-tab') !== activeTab)) {
+    const setCurrent = function(tab, content) {
+      tab.classList.toggle('operations__tab--active');
+      content.classList.toggle('operations__content--active')
+    }
+    
+    // deactivate existing view
+    const currentContent = document.querySelector(`.operations__content--${activeTab}`);
+    const currentTab = document.querySelector(`.operations__tab--${activeTab}`);
+    setCurrent(currentTab, currentContent);
+
+
+    // set active tab
+    activeTab = e.target.getAttribute('data-tab');
+
+    // activate new view
+    const newContent = document.querySelector(`.operations__content--${activeTab}`);
+    const newTab = document.querySelector(`.operations__tab--${activeTab}`);
+    setCurrent(newTab, newContent);
+
+
+  }
+});
+
+const handleHover = function(e) {
+  if(e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+// menu fade animation
+const nav = document.querySelector('.nav');
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+const initialCoords = section1.getBoundingClientRect();
+const navHeight = nav.getBoundingClientRect();
+
+nav.addEventListener('mouseout', handleHover.bind(1.0));
+
+
+// Not a good implementation
+// window.addEventListener('scroll', (e) => {
+//   const currentPos = window.scrollY;
+//   console.log(currentPos, initialCoords.top);
+//   if (currentPos >= initialCoords.top) {
+//     nav.classList.add('sticky');
+//   } else {
+//     nav.classList.remove('sticky');
+//   }
+
+// });
+
+const stickyNav = function(entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+}
+
+const obsOpts = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight.height}px`
+}
+
+const sections = document.querySelectorAll('.section');
+
+
+
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, obsOpts);
+headerObserver.observe(header);
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15
+});
+
+sections.forEach(section => {
+  section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+  
+})
+
+const loadImg = function(entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  console.log(entry.target);
+  entry.target.addEventListener('load', (e) => {
+    entry.target.classList.remove('lazy-img');
+  });
+  imgObserver.unobserve(entry.target);
+  
+}
+
+const imageTargets = document.querySelectorAll('img')
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0
+});
+
+imageTargets.forEach(img => imgObserver.observe(img));
+
+const btnRight = document.querySelector('.slider__btn--right');
+const btnLeft = document.querySelector('.slider__btn--left');
+
+const slides = document.querySelectorAll('.slide');
+
+const slider = document.querySelectorAll('.slider');
+
+const dotContainer = document.querySelector('.dots');
+
+let curSlide = 0;
+let maxSlide = slides.length - 1;
+
+const createDots =function() {
+  slides.forEach((_, i) => {
+    dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`);
+  })
+}
+
+const setActiveDot = function() {
+  document.querySelectorAll('.dots__dot').forEach((dot, i) => {
+    if (i === curSlide) {
+      dot.classList.add('dots__dot--active');
+    } else {
+      dot.classList.remove('dots__dot--active');
+    }
+  })
+}
+
+const setSlides = function() {
+  slides.forEach((s,i) => s.style.transform = `translateX(${100 * (i - curSlide)}%)`);
+  setActiveDot();
+}
+
+createDots();
+setSlides();
+
+
+
+const nextSlide = function() {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  setSlides();
+}
+
+const prevSlide = function() {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+  setSlides();
+}
+
+btnRight.addEventListener('click', nextSlide);
+
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+})
+
+dotContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('dots__dot')) {
+    const {slide} = e.target.dataset;
+    curSlide = Number(slide);
+    setSlides();
+  }
+})
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,295 @@
 'use strict';
+'use strict';
+
+// Produces a person object
+const Person = function(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // return this;
+}
+
+const morris = new Person('Morris', 1976);
+const jonas = new Person('Jonas', 1992);
+const matilda = new Person('Matilda', 1975);
+const jack = new Person('Jack', 2017);
+// Prototypes
+
+Person.prototype.calcAge = function() {
+    return new Date().getFullYear() - this.birthYear;
+}
+
+const Student = function(firstName, birthYear, course) {
+    Person.call(this, firstName, birthYear);
+    this.course = course;
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.introduce = function() {
+    console.log(`Hello class, my name is ${this.firstName}. I am studying ${this.course}. I am ${this.calcAge()} years old!`);
+}
+
+console.log(jack.calcAge());
+
+const mike = new Student('Mike', 2000, 'Computer Science');
+
+mike.introduce();
+
+
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car1 = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+Car1.prototype.accelerate = function() {
+    this.speed += 10;
+    this.displaySpeed();
+}
+
+Car1.prototype.brake = function() {
+    this.speed -= 5;
+    this.displaySpeed();
+}
+
+Car1.prototype.displaySpeed = function() {
+    console.log(`${this.make} is traveling at ${this.speed} km/h`);
+}
+
+const bmw1 = new Car1('BMW', 120);
+
+const mercedes1 = new Car1('Mercedes', 95);
+
+
+class PersonClass {
+    constructor(name, birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    calcAge() {
+        console.log(new Date().getFullYear() - this.birthYear)
+    }
+
+    greet() {
+        console.log(`Hey ${this.name}!`)
+    }
+
+    get age() {
+        return this.calcAge();
+    }
+}
+
+class StudentClass extends PersonClass {
+    constructor(name, birthYear, course) {
+        super(name, birthYear);
+        this.course = course;
+    }
+    introduce() {
+        console.log(`Hello class, my name is ${this.name}. I am studying ${this.course}. I am ${this.calcAge()} years old!`);
+    }
+
+    calcAge() {
+        console.log(`I am ${new Date().getFullYear() - this.birthYear} years old, but as a student I feel more like ${new Date().getFullYear() - this.birthYear + 10}`)
+    }
+}
+
+const martha = new StudentClass('Martha Jones', 2012, 'Computer Science');
+
+martha.introduce();
+
+const rachel = new PersonClass('Jessica', 1996);
+
+const account = {
+    owner: 'Jonas',
+    movements: [200, 530, 120, 300],
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+    set latest(mov) {
+        this.movements.push(mov);
+    }
+
+}
+
+console.log(account.latest);
+
+const PersonProto = {
+    calcAge() {
+        console.log(new Date().getFullYear() - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+}
+
+const sarah = Object.create(PersonProto);
+
+sarah.init('Sarah', 1979);
+sarah.calcAge();
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+// DO the following with ES6 class structure
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+class Car {
+    make = '';
+    speed = 0;
+    
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        this.displaySpeed();
+    }
+
+    brake = function() {
+        this.speed -= 5;
+        this.displaySpeed();
+    }
+
+    displaySpeed = function() {
+        console.log(`${this.make} is traveling at ${this.speed} km/h`);
+    }
+
+    get speedUS() {
+        return this.speed / 1.6;
+    }
+
+
+
+}
+
+const bmw = new Car('BMW', 120);
+
+const mercedes = new Car('Mercedes', 95);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class EV extends Car {
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.charge = charge;
+    }
+    chargeBattery(chargeTo) {
+        this.charge = chargeTo;
+    }
+    accelerate() {
+        this.speed += 10;
+        this.charge--;
+        console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge} %`);
+        this.displaySpeed();
+    }
+
+}
+
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.accelerate();
+
+tesla.brake();
+tesla.brake();
+tesla.accelerate();
+
+let arr = [1,2,3,4];
+
+let result = 0;
+
+for (let number of arr) {
+    result *= number;
+}
+console.log(result);
+
+class Account {
+    _movements = [];
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.pin = pin;
+        this.locale = navigator.language;
+
+        console.warn('Thanks for opening your account');
+    }
+
+    getMovements() {
+        return this._movements;
+    }
+
+    deposit(val) {
+        this._movements.push(val);
+        return this;
+    }
+
+    withdraw(val) {
+        return this.deposit(-val);
+    }
+
+    approveLoan() {
+        return true;
+    }
+
+    requestLoan(val) {
+        if (this.approveLoan()) {
+            return this.deposit(val);
+        }
+    }
+
+}
+
+const account1 = new Account('Jonas', "EUR", 1111);
+
+account1.deposit(4340);
+account1.withdraw(34);
+
+
+
+account1.deposit(400).deposit(433).withdraw(43).requestLoan(1000);
+console.log(account1.getMovements());
